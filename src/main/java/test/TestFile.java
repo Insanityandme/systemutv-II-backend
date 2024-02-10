5d375f4e92efe59823908786ca18c13e4920446c
gihedenalexander <giheden.alexander@gmail.com>
2024-02-10 12:58:34 +0100

    Added dependencies to maven and prepared a test folder for future tests

---
+++ src/main/java/test/TestFile.java
@@ -0,0 +1,12 @@
+package test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestFile {
+
+    @Test
+    public void test1() { //Bara så länge
+        assertEquals(1, 1);
+    }
+}
