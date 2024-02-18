622850d41a0c95d098f9dc1fea2ef07f372a3188
Dino Patarcec <61086008+Dinomaster818@users.noreply.github.com>
2024-02-18 17:44:57 +0100

    Update UserDeleteAccTest.java

--- src/main/testing/UserDeleteAccTest.java
+++ src/main/testing/UserDeleteAccTest.java
@@ -1,2 +1,73 @@
+import io.javalin.Javalin;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import se.myhappyplants.server.services.IQueryExecutor;
+import se.myhappyplants.server.services.UserRepository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 public class UserDeleteAccTest {
+
+    @Mock
+    private IQueryExecutor database;
+
+    @InjectMocks
+    private UserRepository userRepository;
+
+    private static Javalin app;
+
+    @BeforeAll
+    public static void setUp() {
+        app = Javalin.create().start(7000);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        app.stop();
+    }
+
+    @Test
+    public void testDeleteAccountSuccess() throws SQLException {
+        when(userRepository.checkLogin(any(), any())).thenReturn(true);
+
+        PreparedStatement preparedStatementMock = mock(PreparedStatement.class);
+        when(database.prepareStatement(any(String.class))).thenReturn(preparedStatementMock);
+        when(preparedStatementMock.executeUpdate()).thenReturn(1);
+
+        ResultSet resultSetMock = mock(ResultSet.class);
+        when(resultSetMock.next()).thenReturn(true);
+        when(resultSetMock.getInt("id")).thenReturn(1);
+        when(preparedStatementMock.executeQuery()).thenReturn(resultSetMock);
+
+        assertTrue(userRepository.deleteAccount("test@example.com", "password"));
+
+        verify(userRepository, times(1)).checkLogin(any(), any());
+        verify(database, times(2)).prepareStatement(any(String.class));
+        verify(resultSetMock, times(1)).next();
+        verify(resultSetMock, times(1)).getInt("id");
+        verify(preparedStatementMock, times(2)).executeUpdate();
+        verify(database, times(1)).beginTransaction();
+        verify(database, times(1)).endTransaction();
+    }
+
+    @Test
+    public void testDeleteAccountFailure() throws SQLException {
+        when(userRepository.checkLogin(any(), any())).thenReturn(false);
+
+        assertFalse(userRepository.deleteAccount("test@example.com", "password"));
+
+        verify(userRepository, times(1)).checkLogin(any(), any());
+        verify(database, never()).prepareStatement(any(String.class));
+        verify(database, never()).beginTransaction();
+        verify(database, never()).endTransaction();
+    }
 }
