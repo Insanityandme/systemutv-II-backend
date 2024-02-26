b870364b942996c700b06fbef3d84666199dafef
Dino Patarcec <61086008+Dinomaster818@users.noreply.github.com>
2024-02-26 18:18:19 +0100

    Create UserDeleteAccTest.java

---
+++ src/main/testing/UserDeleteAccTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+import se.myhappyplants.javalin.Javalin;
+import se.myhappyplants.javalin.user.NewDeleteRequest;
+
+import io.javalin.http.Context;
+
+import org.junit.jupiter.api.*;
+import se.myhappyplants.javalin.utils.DbConnection;
+
+import java.sql.Connection;
+
+import static org.mockito.Mockito.*;
+
+
+public class UserDeleteAccTest {
+
+    private final Context ctx = mock(Context.class);
+
+    @BeforeEach
+    public void setUp() {
+        DbConnection mockDB = mock(DbConnection.class);
+        when(DB)
+
+
+
+    }
+
+
+    @Test
+    public void DELETE_userDeleteAcc_204_Success(){
+        NewDeleteRequest del = new NewDeleteRequest();
+        del.password="test";
+
+        when(ctx.bodyAsClass(NewDeleteRequest.class)).thenReturn(del);
+        doReturn("13").when(ctx).pathParam("id");
+
+        Javalin.deleteUser(ctx);
+
+        verify(ctx).status(204);
+
+    }
+
+    @Test
+    public void DELETE_userDeleteAcc_400_Fail(){
+        NewDeleteRequest del = new NewDeleteRequest();
+        del.password="wrong_password";
+
+        when(ctx.bodyAsClass(NewDeleteRequest.class)).thenReturn(del);
+        doReturn("11").when(ctx).pathParam("id");
+
+        Javalin.deleteUser(ctx);
+
+        verify(ctx).status(400);
+
+    }
+
+
+}
