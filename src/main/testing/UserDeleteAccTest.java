2aac62f0960e25ee1d7fc2c72ad1ae85f93c576d
Dino Patarcec <61086008+Dinomaster818@users.noreply.github.com>
2024-02-27 15:49:42 +0100

    Update UserDeleteAccTest.java

--- src/main/testing/UserDeleteAccTest.java
+++ src/main/testing/UserDeleteAccTest.java
@@ -5,21 +5,44 @@ import se.myhappyplants.javalin.user.NewDeleteRequest;
 import io.javalin.http.Context;
 
 import org.junit.jupiter.api.*;
+import se.myhappyplants.javalin.user.NewUserRequest;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.mockito.Mockito.*;
 
 public class UserDeleteAccTest {
     private final Context ctx = mock(Context.class);
+    private String email;
+    private String username;
+    private String password;
 
     @BeforeEach
     public void setUp() throws SQLException {
+        email = generateRandomString(8) + "@example.com";
+        username = generateRandomString(10);
+        password = generateRandomString(12);
+        NewUserRequest testUser = new NewUserRequest();
 
+    }
+
+    private String generateRandomString(int length) {
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder randomString = new StringBuilder();
+
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            randomString.append(characters.charAt(index));
+        }
+
+        return randomString.toString();
     }
 
     @Test
     public void DELETE_userDeleteAcc_204_Success() {
+
         NewDeleteRequest del = new NewDeleteRequest();
         del.password = "123"; //Might have to be changed depending on the actual user id you wanna test
 
